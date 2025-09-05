e73a2e33fa99102db4ea246ca34ce68aee4db298
GoncaloFigueiredo <figueiredogoncalo024@gmail.com>
2025-09-05 15:24:39 +0100

    Adicionar arquivos de projeto.

---
+++ AppGestorTarefas/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿Imports System.Drawing.Drawing2D
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        NameLabel = New TextBox()
+        EmailLabel = New TextBox()
+        PasswordLabel = New TextBox()
+        Label2 = New Label()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Button1 = New Button()
+        checkPass = New CheckBox()
+        TextBox1 = New TextBox()
+        erroNome = New Label()
+        erroEmail = New Label()
+        erroPassword = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(217, 128)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 0
+        ' 
+        ' NameLabel
+        ' 
+        NameLabel.Location = New Point(172, 146)
+        NameLabel.Name = "NameLabel"
+        NameLabel.PlaceholderText = "Introduza o seu nome"
+        NameLabel.Size = New Size(205, 23)
+        NameLabel.TabIndex = 1
+        ' 
+        ' EmailLabel
+        ' 
+        EmailLabel.Location = New Point(172, 213)
+        EmailLabel.Name = "EmailLabel"
+        EmailLabel.PlaceholderText = "Introduza o seu email"
+        EmailLabel.Size = New Size(205, 23)
+        EmailLabel.TabIndex = 3
+        ' 
+        ' PasswordLabel
+        ' 
+        PasswordLabel.Location = New Point(172, 273)
+        PasswordLabel.Name = "PasswordLabel"
+        PasswordLabel.PlaceholderText = "Introduza a palavra-passe"
+        PasswordLabel.Size = New Size(205, 23)
+        PasswordLabel.TabIndex = 4
+        PasswordLabel.UseSystemPasswordChar = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(242, 54)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(57, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "Welcome"
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.BorderStyle = BorderStyle.None
+        TextBox5.Location = New Point(172, 193)
+        TextBox5.Name = "TextBox5"
+        TextBox5.PlaceholderText = "Nome"
+        TextBox5.Size = New Size(61, 16)
+        TextBox5.TabIndex = 8
+        TextBox5.Text = "Email"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.BorderStyle = BorderStyle.None
+        TextBox6.Location = New Point(172, 256)
+        TextBox6.Name = "TextBox6"
+        TextBox6.PlaceholderText = "Nome"
+        TextBox6.Size = New Size(61, 16)
+        TextBox6.TabIndex = 9
+        TextBox6.Text = "Password"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.MenuBar
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Location = New Point(232, 328)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(97, 24)
+        Button1.TabIndex = 10
+        Button1.Text = "Registar"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' checkPass
+        ' 
+        checkPass.AutoSize = True
+        checkPass.Location = New Point(383, 275)
+        checkPass.Name = "checkPass"
+        checkPass.Size = New Size(54, 19)
+        checkPass.TabIndex = 11
+        checkPass.Text = "show"
+        checkPass.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Location = New Point(172, 128)
+        TextBox1.Name = "TextBox1"
+        TextBox1.PlaceholderText = "Nome"
+        TextBox1.Size = New Size(61, 16)
+        TextBox1.TabIndex = 12
+        TextBox1.Text = "Nome"
+        ' 
+        ' erroNome
+        ' 
+        erroNome.AutoSize = True
+        erroNome.ForeColor = Color.Red
+        erroNome.Location = New Point(170, 172)
+        erroNome.Name = "erroNome"
+        erroNome.Size = New Size(0, 15)
+        erroNome.TabIndex = 16
+        ' 
+        ' erroEmail
+        ' 
+        erroEmail.AutoSize = True
+        erroEmail.ForeColor = Color.Red
+        erroEmail.Location = New Point(170, 238)
+        erroEmail.Name = "erroEmail"
+        erroEmail.Size = New Size(0, 15)
+        erroEmail.TabIndex = 17
+        ' 
+        ' erroPassword
+        ' 
+        erroPassword.AutoSize = True
+        erroPassword.ForeColor = Color.Red
+        erroPassword.Location = New Point(170, 299)
+        erroPassword.Name = "erroPassword"
+        erroPassword.Size = New Size(0, 15)
+        erroPassword.TabIndex = 18
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonHighlight
+        ClientSize = New Size(548, 520)
+        Controls.Add(erroPassword)
+        Controls.Add(erroEmail)
+        Controls.Add(erroNome)
+        Controls.Add(TextBox1)
+        Controls.Add(checkPass)
+        Controls.Add(Button1)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(Label2)
+        Controls.Add(PasswordLabel)
+        Controls.Add(EmailLabel)
+        Controls.Add(NameLabel)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NameLabel As TextBox
+    Friend WithEvents EmailLabel As TextBox
+    Friend WithEvents PasswordLabel As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents checkPass As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents erroNome As Label
+    Friend WithEvents erroEmail As Label
+    Friend WithEvents erroPassword As Label
+
+End Class
---
+++ AppGestorTarefas/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs)
+
+    End Sub
+
+    'validacoes para o nome do utilizador
+    Private Sub NameLabel_TextChanged(sender As Object, e As EventArgs) Handles NameLabel.Leave
+        Dim nome As String = NameLabel.Text.Trim()
+
+        ' Limpa mensagem de erro
+        erroNome.Text = ""
+
+        ' Validação de tamanho
+        If nome.Length < 3 Or nome.Length > 20 Then
+            erroNome.Text = "O nome precisa de ter entre 3 e 20 caracteres"
+        ElseIf Not Regex.IsMatch(nome, "^[a-zA-Z ]+$") Then
+            erroNome.Text = "O nome não pode conter números ou caracteres especiais."
+        End If
+
+    End Sub
+
+    'validacoes para o email do utilizador
+    Private Sub EmailLabel_TextChanged(sender As Object, e As EventArgs) Handles EmailLabel.Leave
+        Dim email As String = EmailLabel.Text.Trim()
+
+        erroEmail.Text = ""
+
+        If String.IsNullOrEmpty(email) Then
+            erroEmail.Text = "Por favor , preencha este campo"
+        ElseIf Not Regex.IsMatch(email, "^[^@\s]+@[^@\s]+\.[^@\s]+$") Then
+            erroEmail.Text = "Email inválido , tente novamente"
+        End If
+
+    End Sub
+
+    'validacoes para a password
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles PasswordLabel.Leave
+        Dim password As String = PasswordLabel.Text.Trim()
+
+        erroPassword.Text = ""
+
+        If String.IsNullOrEmpty(password) Then
+            erroPassword.Text = "Por favor , introduza a sua password"
+        ElseIf Not Regex.IsMatch(password, "^[a-zA-Z0-9!@#$%&*()_+\-=\.,?]+$") Then
+            erroPassword.Text = "Por favor , introduza uma password válida"
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles checkPass.CheckedChanged
+        If checkPass.Checked = True Then
+            PasswordLabel.UseSystemPasswordChar = False
+        Else
+            PasswordLabel.UseSystemPasswordChar = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nome As String = NameLabel.Text.Trim()
+        Dim email As String = EmailLabel.Text.Trim()
+        Dim password As String = PasswordLabel.Text.Trim()
+    End Sub
+
+    Private Sub erroPassword_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+End Class
